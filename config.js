380c6bc84733260cee9688aa7279146a0a67e55c
mileslukas <mileslukas@Miless-MacBook-Pro.local>
2013-05-14 11:49:32 -0500

    first

---
+++ config.js
@@ -0,0 +1,3 @@
+	
+
+var standalone = "no";
---
+++ scripts/richMedia/data.js
@@ -0,0 +1,244 @@
+var admaxim_ad =
+{
+	"resources":
+	{
+		"adid"				: "ad_template_id",
+		"adgroupid"			: "ad_template_group",
+		"showhotspots"		: "no",
+		"enabletracking"	: "yes",
+		"startingpage"		: "0",
+		"width"				: "320",
+		"height"			: "460",
+		"menuheight"		: "50",
+		'gallery':
+		{
+			'pages':
+			[	
+			 	{
+				 	'title':'page1',
+				 	'track':'page_1_view',
+					'track2'	: ' ',
+				 	'background':'media/bg_home.png',
+				 	"html"		: 
+					[
+						{
+							"embed" : "<p style='position:absolute; color:orange; top:34px; text-align:center; width:320px'>CUSTOM HTML HEADER</p>"
+						}
+					],
+					'video'		:
+					[
+						{
+							'width'	: '270',
+							'height': '180',
+							'x' 	: '25',
+							'y' 	: '140',
+							'url' 	: 'media/logo.mp4',
+							'track' : { 'play' : 'play_video', 'pause' : 'pause_video', 'percent25'	: '25_percent_video', 'percent50' : '50_percent_video', 'percent75'	: '75_percent_video', 'ended' : 'ended_video' },
+							'track2': { 'play' : '', 'pause' : '', 'percent25' : '', 'percent50' : '', 'percent75' : '', 'ended' : '' }
+						}
+					],
+					"links" :	
+					[
+					 	{
+					 		"type"		: "fb_share",
+							"height"	: "30",
+							"width"		: "30",
+							"url" 		: "http://admaxim.com",
+							"x" 		: "26",
+							"y" 		: "330",
+							"background": "media/btn_facebook.png",
+							"track" 	: "facebook_click",
+							"track2"	: "",
+						},
+						{
+							"type"		: "twt_share",
+							"height"	: "30",
+							"width"		: "30",
+							"url" 		: "http://admaxim.com",
+							"x" 		: "266",
+							"y" 		: "330",
+							"background": "media/btn_twitter.png",
+							"track" 	: "twitter_click",
+							"track2"	: ""
+						},
+						{
+							"type"		: "custom",
+							"height"	: "44",
+							"width"		: "214",
+							"url" 		: "http://admaxim.com",
+							"x" 		: "44",
+							"y" 		: "81",
+							"track" 	: "custom_link_click",
+							"track2"	: ""
+						}
+					]
+				},
+				{
+				 	'title':'page2',
+				 	'track':'page_2_view',
+					'track2'	: ' ',
+				 	'background':'media/bg_home.png',
+				 	"html"		: 
+					[
+						{
+							"embed" : "<p style='position:absolute; color:orange; top:34px; text-align:center; width:320px'>CUSTOM HTML HEADER</p>"
+						}
+					],
+					'video'		:
+					[
+						{
+							'width'	: '270',
+							'height': '180',
+							'x' 	: '25',
+							'y' 	: '140',
+							'url' 	: 'media/logo.mp4',
+							'track' : { 'play' : 'play_video', 'pause' : 'pause_video', 'percent25'	: '25_percent_video', 'percent50' : '50_percent_video', 'percent75'	: '75_percent_video', 'ended' : 'ended_video' },
+							'track2': { 'play' : '', 'pause' : '', 'percent25' : '', 'percent50' : '', 'percent75' : '', 'ended' : '' }
+						}
+					],
+					"links" :	
+					[
+					 	{
+					 		"type"		: "fb_share",
+							"height"	: "30",
+							"width"		: "30",
+							"url" 		: "http://admaxim.com",
+							"x" 		: "26",
+							"y" 		: "330",
+							"background": "media/btn_facebook.png",
+							"track" 	: "facebook_click",
+							"track2"	: "",
+						},
+						{
+							"type"		: "twt_share",
+							"height"	: "30",
+							"width"		: "30",
+							"url" 		: "http://admaxim.com",
+							"x" 		: "266",
+							"y" 		: "330",
+							"background": "media/btn_twitter.png",
+							"track" 	: "twitter_click",
+							"track2"	: ""
+						},
+						{
+							"type"		: "custom",
+							"height"	: "44",
+							"width"		: "214",
+							"url" 		: "http://admaxim.com",
+							"x" 		: "44",
+							"y" 		: "81",
+							"track" 	: "custom_link_click",
+							"track2"	: ""
+						}
+					]
+				},
+				{
+				 	'title':'page3',
+				 	'track':'page_3_view',
+					'track2'	: ' ',
+				 	'background':'media/bg_home.png',
+				 	"html"		: 
+					[
+						{
+							"embed" : "<p style='position:absolute; color:orange; top:34px; text-align:center; width:320px'>CUSTOM HTML HEADER</p>"
+						}
+					],
+					'video'		:
+					[
+						{
+							'width'	: '270',
+							'height': '180',
+							'x' 	: '25',
+							'y' 	: '140',
+							'url' 	: 'media/logo.mp4',
+							'track' : { 'play' : 'play_video', 'pause' : 'pause_video', 'percent25'	: '25_percent_video', 'percent50' : '50_percent_video', 'percent75'	: '75_percent_video', 'ended' : 'ended_video' },
+							'track2': { 'play' : '', 'pause' : '', 'percent25' : '', 'percent50' : '', 'percent75' : '', 'ended' : '' }
+						}
+					],
+					"links" :	
+					[
+					 	{
+					 		"type"		: "fb_share",
+							"height"	: "30",
+							"width"		: "30",
+							"url" 		: "http://admaxim.com",
+							"x" 		: "26",
+							"y" 		: "330",
+							"background": "media/btn_facebook.png",
+							"track" 	: "facebook_click",
+							"track2"	: "",
+						},
+						{
+							"type"		: "twt_share",
+							"height"	: "30",
+							"width"		: "30",
+							"url" 		: "http://admaxim.com",
+							"x" 		: "266",
+							"y" 		: "330",
+							"background": "media/btn_twitter.png",
+							"track" 	: "twitter_click",
+							"track2"	: ""
+						},
+						{
+							"type"		: "custom",
+							"height"	: "44",
+							"width"		: "214",
+							"url" 		: "http://admaxim.com",
+							"x" 		: "44",
+							"y" 		: "81",
+							"track" 	: "custom_link_click",
+							"track2"	: ""
+						}
+					]
+				}						 
+			]
+		},
+		'menu':
+		{
+			'buttons':
+			[
+			  	{
+				   	'type':0,
+				  	'track':'page1_menu',
+					'track2':'',
+					'icon':'media/icon_games.png'
+				},
+				{
+				   	'type':1,
+				  	'track':'page2_menu',
+					'track2':'',
+					'icon':'media/icon_video.png'
+				},
+				{
+				   	'type':2,
+				  	'track':'page3_menu',
+					'track2':'',
+					'icon':'media/icon_share.png'
+				},
+				{
+				   	'type':'close',
+				  	'track':'close_btn',
+					'track2':'',
+					'icon':'media/icon_close.png'
+				}  
+			],
+			'selected' :
+			{
+				'opacity':'1',
+				'background':'media/menu_highlight.png'
+			},
+			'menubg':'media/menu_bg.png'
+		},
+		'additionaltracking':
+		{
+			'firstexpand'	: 'page_impression',
+			'firstexpand2'	: 'page_impression2',
+			'expand'		: 'expand_ad',
+			'expand2'		: 'expand_ad2',
+			'swipeleft'		: 'swipe_left',
+			'swipeleft2'	: 'swipe_left2',
+			'swiperight'	: 'swipe_right',
+			'swiperight2'	: 'swipe_right2'
+		}
+	}
+}
